93781e4d914354feed921fdc0a8175412057411e
Anna <ania.pwk.ap@gmail.com>
2024-02-27 22:28:06 +0100

    JS cleanup

--- projects/project.js
+++ projects/project.js
@@ -1,11 +1,3 @@
-// // •	Using this endpoint, fetch the first three projects of the API to display on the website in the expected position. This API returns the projects in descending order, so you can assume the first item of the array is the last project.
-// o	Create a folder named projects with a file named 1.html inside.
-// o	Inside this file, you should fetch the API using the onload event. Use MDN - Window: load event if you need help
-// o	Filter the project with uuid 1 and print the name, description, content, image and completed_on in the expected position of the page.
-// o	Other projects should be other projects of the API. For example, if you are filtering the project with uuid 1, other projects must be others.
-// o	BONUS: Take three other projects randomly. If the project does not exist in the API, you should alert the user.
-// //
-
 function getProjectIdFromURL() {
 	const searchParams = window.location.search;
 	const params = new URLSearchParams(searchParams);
@@ -75,10 +67,10 @@ function getOtherProjects(projects, id) {
 	for (let i = 0; i < projects.length; i++) {
 		if (id !== projects[i].uuid) othProjects.push(projects[i]);
 	}
-	console.log("we are in");
-	console.dir(othProjects);
+
 	return othProjects;
 }
+
 window.onload = async () => {
 	const id = getProjectIdFromURL();
 	const projects = await fetchAllProjects();
